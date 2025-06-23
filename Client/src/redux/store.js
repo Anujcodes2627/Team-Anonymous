35e2aa05badc1d583a23d2a6ca46ec1070b6e32c
Anuj Namdev <anujnamdev36385@gmail.com>
2025-06-23 18:12:35 +0530

    Store Configuration User Actions in Server And Client Testing Left

---
+++ Client/src/redux/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit"
+import userSlice from "./user/user.reducer"
+
+export const store =configureStore({
+    reducer:{
+        user:userSlice,
+    }
+})
