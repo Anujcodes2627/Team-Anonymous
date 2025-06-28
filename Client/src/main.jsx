1a01bfec4576158703f167ad87319cf4f54010c3
Anuj Namdev <anujnamdev36385@gmail.com>
2025-06-28 11:36:53 +0530

    error solved

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -13,26 +13,24 @@ import store from "./redux/store.js";
 import Home from "./Pages/Home.jsx";
 import About from "./Pages/About.jsx";
 import Layout from "./Layout.jsx";
-<<<<<<< HEAD
+
 import LoginPage from "./Pages/LoginPage.jsx";
 import SignUp from "./Pages/SignUp.jsx";
-=======
+
 import Dashboard from "./Pages/Dashboard.jsx";
 // import Contact from "./Pages/Contact.jsx";
->>>>>>> 46a8894192006719efc9456473f42d5c37b1a3fe
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-<<<<<<< HEAD
+
       <Route path="login" element={<LoginPage />} />
       <Route path="signup" element={<SignUp />} />
-=======
+
       {/* <Route path="contact" element={<Contact />} /> */}
       <Route path="dashboard" element={<Dashboard />} />
->>>>>>> 46a8894192006719efc9456473f42d5c37b1a3fe
     </Route>
   )
 );
